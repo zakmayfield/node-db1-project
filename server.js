c92c82d2a0d731cfac4c6203b5699749bf3e9d98
Zachary Mayfield <zakmayfield@gmail.com>
2020-02-17 15:43:48 -0500

    MVP MET
    wrote CRUD endpoints for the accounts resource / tested endpoints with postman

--- server.js
+++ server.js
@@ -1,9 +1,16 @@
 const express = require('express');
-
+const AccountsRouter = require('./accounts/accounts-router.js');
 const db = require('./data/dbConfig.js');
 
 const server = express();
 
 server.use(express.json());
+server.use('/api/accounts', AccountsRouter);
+
+
+server.get('/', (req, res) => {
+  res.send('<h1>DB Running</h1>')
+})
+
 
 module.exports = server;
